88af0843d3068686b0f5fd102d61c812a8ab5b49
didillysquat <didillysquat@gmail.com>
2019-09-03 17:08:29 +0300

    end of day. implementing transition from relative to absolute data.

--- symportal_website/data_dev/data_dev.js
+++ symportal_website/data_dev/data_dev.js
@@ -1,9 +1,17 @@
 // d3.json("data/seqs.absolute.json").then(d => chart(d))
-var json_data = getSeqData()
+//TODO list includes:
+// Have a toggle for absolute/relative
+// Have a toggle for pre-MED/post-MED
 
-chart(json_data);
 
-function chart(data) {
+
+chart();
+
+function chart() {
+    //This initial chart function will take care of all of the svg elements that are not going to change during
+    // an update. i.e. when we are changing from relative to absolute data
+
+    // Here we set the margins of the svg chart
     var svg = d3.select("#chart"),
 		margin = {top: 35, left: 35, bottom: 20, right: 0},
 		width = +svg.attr("width") - margin.left - margin.right,
@@ -19,10 +27,16 @@ function chart(data) {
         .rangeRound([height - margin.bottom, margin.top])
 
     // Set the colour scale
+    // We can set both the range and domain of this as these are invariable between absolute and relative
+    // data types
+    var c_dict_post_med = getColDictObjArr()
+    var c_dict_post_med_colors = c_dict_post_med.map(d => d.d_value)
+    var c_dict_post_med_seq_names = c_dict_post_med.map(d => d.d_key)
     var col_scale = d3.scaleOrdinal()
-    .range(["steelblue", "darkorange", "lightblue"])
-    .domain([0, 1, 2]);
+    .range(c_dict_post_med_colors)
+    .domain(c_dict_post_med_seq_names);
         
+    //Set up the svg element in which we will call the axis objects
     var xAxis = svg.append("g")
     .attr("transform", `translate(0,${height - margin.bottom})`)
     .attr("class", "x-axis")
@@ -31,82 +45,94 @@ function chart(data) {
 		.attr("transform", `translate(${margin.left},0)`)
         .attr("class", "y-axis")
     
-    // A regex that will allow us to extract the names of the sequences
-    var regex = new RegExp('(^[ABCDEFGHI]|_[ABCDEFGHI]$)', 'g');
-    // Get an array that is all of the names of the sequence
-    var seq_names = Object.keys(data[0]).filter(property => property.match(regex));
+    update(d3.select("#data_type_selector").property("value"), 0);
 
-    // we can get the total from the property 'post_med_absolute'
+    function update(data_type, speed){
+        if (data_type == 'absolute'){
+            var data  = getSeqDataAbsolutePostMED();
+            // if plotting absolute values we can get the highest y from the 'post_med_aboslute' property
+            var max_y_val = d3.max(data, d => d.post_med_absolute);
+        } else if (data_type == 'relative')
+        {
+            var data = getSeqDataRelativePostMED();
+            var max_y_val = 1;
+        }
 
-    // Find the largest value and use this to set the domain for the y scale
-    // This can be moved up to be chined onto the y scale where it is created
-    max_y_val = d3.max(data, d => d.post_med_absolute)
-    y.domain([0, max_y_val]).nice();
+        // Update the Y scale's domain depending on whether we are doing absolute or relative data_type
+        y.domain([0, max_y_val]).nice();
 
-    // Complete the y axis
-    svg.selectAll(".y-axis")
-    .transition()
-    .duration(0)
-    .call(d3.axisLeft(y)
-    .ticks(null, "s"));
-    
-    // Set the domain of the x. This should be the sample names
-    // We should be able to move this up similar to the y domain def
-    x.domain(data.map(d => d.sample_name));
+        // Now update the y axis
+        svg.selectAll(".y-axis")
+        .transition()
+        .duration(speed)
+        .call(d3.axisLeft(y)
+        .ticks(null, "s"));
 
-    // Complete the x axis
-    svg.selectAll(".x-axis").transition().duration(0)
-            .call(d3.axisBottom(x).tickSizeOuter(0)).selectAll("text").attr("y", 0).attr("x", 9).attr("dy", ".35em").attr("transform", "rotate(90)").style("text-anchor", "start").style("text-anchor", "start");
-            
-    // var x_ax = d3.selectAll(".x-axis");
-    // var x_ax = x_ax.selectAll("text");
-    // var x_ax = x_ax.attr("y", 0);
-    // var x_ax = x_ax.attr("x", 9);
-    // var x_ax = x_ax.attr("dy", ".35em");
-    // var x_ax = x_ax.attr("transform", "rotate(90)");
-    // var x_ax = x_ax.style("text-anchor", "start");
-    
-    // Some magic with stacking
-    // by having the keys provided here, a key property is associated to each of the g.layers created
-    // this is then used when doing the colour lookup for example.
-    var group = svg.selectAll("g.layer")
-    .data(d3.stack().keys(seq_names)(data), d => d.key)
+        // Set the domain of the x. This should be the sample names
 
-    // Get an int value that can be used for modulus the colour
-    data.forEach(function(d, i) {
-        d.col_int = i % 3;
-        return d
-    })
+        x.domain(data.map(d => d.sample_name));
 
-    var group = group.enter().append("g")
-    var group = group.classed("layer", true)
-    var group = group.attr("fill", function(d){
-        // d.key gives us the user 
-        // col_scale(d.key);
-        return col_scale(d.index % 3);
-    } );
-    
-    var bars = svg.selectAll("g.layer").selectAll("rect")
-    .data(d => d, e => e.data.sample_name);
+        // Complete the x axis
+        // This should be fairly invariable for the time being as we aren't playing with the order of the x axis yet
+        svg.selectAll(".x-axis").transition().duration(speed)
+                .call(d3.axisBottom(x).tickSizeOuter(0)).selectAll("text").attr("y", 0).attr("x", 9).attr("dy", ".35em").attr("transform", "rotate(90)").style("text-anchor", "start").style("text-anchor", "start");
 
-    var bars = bars.enter().append("rect");
-    var bars = bars.attr("width", x.bandwidth());
-    var bars = bars.merge(bars).transition().duration(0);
-    var bars = bars.attr("x", function(d){
-        var foo = d;
-        return x(d.data.sample_name);
-    });
-    var bars = bars.attr("y", function(d)  {
-        return y(d[1]);
-    });
-    var bars = bars.attr("height", function(d) {
-        var height_here = y(d[0]) - y(d[1]);
-        console.log("Height set to" + height_here);
-        return y(d[0]) - y(d[1]);
-    } );
+        // var x_ax = d3.selectAll(".x-axis");
+        // var x_ax = x_ax.selectAll("text");
+        // var x_ax = x_ax.attr("y", 0);
+        // var x_ax = x_ax.attr("x", 9);
+        // var x_ax = x_ax.attr("dy", ".35em");
+        // var x_ax = x_ax.attr("transform", "rotate(90)");
+        // var x_ax = x_ax.style("text-anchor", "start");
+
+        // Some magic with stacking
+        // by having the keys provided here, a key property is associated to each of the g.layers created
+        // this is then used when doing the colour lookup for example.
+        var group = svg.selectAll("g.layer")
+        .data(d3.stack().keys(c_dict_post_med_seq_names)(data), d => d.key)
+
+        // This is still a bit black magic, but esentially the above .data should automatically enter into the
+        // 'update' state. This should modify the elements that already exist and map them to their new data
+        // values if they exist.
+        // Then we should enter the exit state to get rid of old stuff
+        // Then finally we should work with enter() to enter in any new svg elements and attach them to data
+        group.exit().remove()
+        var group = group.enter().append("g")
+        var group = group.classed("layer", true)
+        var group = group.attr("fill", function(d){
+            // here d is a given sequence and the name can be accessed with d.key
+            // have a look in the chrome dev tools when debugging for more info on the structure of d
+            // d.key gives us the user
+            // col_scale(d.key);
+            return col_scale(d.key);
+        } );
+
+        var bars = svg.selectAll("g.layer").selectAll("rect")
+        .data(d => d, e => e.data.sample_name);
+
+        bars.exit().remove()
+
+        var bars = bars.enter().append("rect");
+        var bars = bars.attr("width", x.bandwidth());
+        var bars = bars.merge(bars).transition().duration(speed);
+        var bars = bars.attr("x", function(d){
+            var foo = d;
+            return x(d.data.sample_name);
+        });
+        var bars = bars.attr("y", function(d)  {
+            return y(d[1]);
+        });
+        var bars = bars.attr("height", function(d) {
+            var height_here = y(d[0]) - y(d[1]);
+            console.log("Height set to" + height_here);
+            return y(d[0]) - y(d[1]);
+        });
+    }
 			
 			
-			
+	var data_type_selector = d3.select("#data_type_selector").on("change", function(){
+	    update(this.value, 750)
+	})
 			
     
 }
