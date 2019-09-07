58ad2e99c773b1c1f74db908fec4fdf244d0e0ad
didillysquat <didillysquat@gmail.com>
2019-09-07 16:34:41 +0300

    EOD adding published articles table

--- symportal_website/assets/js/index.js
+++ symportal_website/assets/js/index.js
@@ -21,3 +21,76 @@ function unblur_nav_bar(){
         document.getElementById("nav_cont").setAttribute("class", "nav_content no_blur");
     }
 }
+
+
+function populate_article_table(data){
+
+    // Get the table object to which we append rows and cells
+    var table = document.getElementById('published_articles_table');
+
+    // For each article create row if published
+    data.forEach(function(article, index){
+        // If the article is not yet published, then do not add to table
+        if (article["published"] == "false"){
+            return
+        }
+
+        // If published, add row and cells to row.
+        var tr = document.createElement('tr');
+
+        // The first row needs to be light, so class .row_off
+        // Then alternate to dark and light
+        if (index%2 == 0) {//class needs to be off
+            tr.setAttribute("class", "article_table_row row_off");
+        }else{
+            tr.setAttribute("class", "article_table_row row_on");
+        }
+
+        // Add a cell for each field
+        table_fields = ["article_name", "title", "location", "num_samples",
+                        "additional_markers", "run_type", "article_url", "seq_data_url"];
+
+        table_fields.forEach(function(field_key){
+            // If url then need to make <a> otherwise plain div
+            if (field_key.includes("url")){
+                var td = document.createElement('td');
+                // set the href to the appropriate value
+                if (field_key == "article_url"){
+                    td.innerHTML = "Article URL"
+//                    td.setAttribute("href", article['article_url']);
+//                    var td_div = document.createElement('div')
+//                    td_div.setAttribute("style", "height:100%;width:100%")
+//                    td_div.innerHTML = "Article URL";
+//                    td.appendChild(td_div)
+
+                }else if (field_key == "seq_data_url"){
+                    td.innerHTML = "Seq data URL"
+//                    td.setAttribute("href", article['seq_data_url']);
+//                    var td_div = document.createElement('div')
+//                    td_div.setAttribute("style", "height:100%;width:100%")
+//                    td_div.innerHTML = "Seq data URL";
+//                    td.appendChild(td_div)
+                }
+            }else{
+                // if not a url then we need a normal div
+                var td = document.createElement('td');
+                td.innerHTML = article[field_key];
+            }
+            tr.appendChild(td);
+        });
+        table.appendChild(tr)
+    });
+
+//    return tr;
+
+
+}
+
+
+
+
+
+
+// populate the published articles table
+published_articles = getPublishedArticlesData()
+populate_article_table(published_articles)
