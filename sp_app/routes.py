d42f298d6cb483d7981a21ef8750f12973afbf76
didillysquat <didillysquat@gmail.com>
2019-09-09 17:00:24 +0300

    update .gitignore, add non-bin resources, setup for flask deployment

--- sp_app/routes.py
+++ sp_app/routes.py
@@ -4,4 +4,8 @@ from sp_app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world!"
+    return render_template('index.html')
+
+@app.route('/submit_data_learn_more')
+def submit_data_learn_more():
+    return render_template('submit_data_learn_more.html')
