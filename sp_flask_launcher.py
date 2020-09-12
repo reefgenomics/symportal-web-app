a6c5df116022c09353e40ea0eda914b48eda4982
didillysquat <didillysquat@gmail.com>
2020-09-12 12:24:16 +0300

    hyperlink title

--- sp_flask_launcher.py
+++ sp_flask_launcher.py
@@ -6,3 +6,6 @@ from sp_app.models import User
 @app.shell_context_processor
 def make_shell_context():
     return {'db':db, 'User':User}
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1',port=5000,debug=True,threaded=True)
