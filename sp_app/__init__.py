55b3893f9133947597343045e0c954d2643dc4f8
didillysquat <didillysquat@gmail.com>
2022-07-22 10:08:11 +0000

    set pool_pre_ping True, pool_recycle: 300; set fav_icon; add upload link

--- sp_app/__init__.py
+++ sp_app/__init__.py
@@ -10,7 +10,11 @@ app.config.from_object(Config)
 with open(os.path.join(basedir, 'sp_app', 'password.txt'), 'r') as f:
     password = f.read().rstrip()
 app.config['SQLALCHEMY_BINDS'] = {'symportal_database': f'postgresql://humebc:{password}@134.34.126.43:5432/symportal_database'}
-db = SQLAlchemy(app)
+
+# See here: https://stackoverflow.com/a/45537199/5516420 as a possible answer that we are trying to receiving the following error:
+# from the logs here: /var/log/sp_app/sp_app.err.log error is: psycopg2.DatabaseError: SSL SYSCALL error: Connection timed out
+engine_options = {"pool_pre_ping": True, "pool_recycle": 300}
+db = SQLAlchemy(app, engine_options=engine_options)
 migrate = Migrate(app,db)
 login = LoginManager(app)
 login.login_view = 'login'
