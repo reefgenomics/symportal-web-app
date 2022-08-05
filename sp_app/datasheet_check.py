2a99b544f6505ffc80460e77202c020f518610a6
didillysquat <didillysquat@gmail.com>
2022-08-05 22:56:07 +0200

    fix date format bug

--- sp_app/datasheet_check.py
+++ sp_app/datasheet_check.py
@@ -235,6 +235,11 @@ class DatasheetChecker:
             if current_date_value == "NoData":
                 continue
             if not pd.isnull(current_date_value):
+                # sometime a weird float string was coming in e.g. 20220601.0
+                try:
+                    current_date_value = str(int(float(current_date_value)))
+                except ValueError:
+                    pass
                 if re.findall("[A-Za-z]+", current_date_value):
                     # Then this date is in a bad format
                     # We will try to extract a year and a month from it
